eba56fb7698905d4b51684551e0f82ab44cf0391
Sean Feldman <feldman.sean@gmail.com>
2017-09-21 22:42:10 -0600

    brought all core files into serverless (links)

--- src/NServiceBus.Serverless/Program.cs
+++ src/NServiceBus.Serverless/Program.cs
@@ -1,16 +1,33 @@
 ﻿namespace NServiceBus
 {
-    /// <summary>
-    ///
-    /// </summary>
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Extensibility;
+    using Pipeline;
+    using Settings;
+    using Transport;
+
+    /// <summary></summary>
     public class Program
     {
-        /// <summary>
-        ///
-        /// </summary>
-        public static void Main()
+        /// <summary></summary>
+        public static async Task Main()
         {
+            var builder = new CommonObjectBuilder(new LightInjectObjectBuilder());
+            var eventAggregator = new EventAggregator(new NotificationSubscriptions());
+            var settingsHolder = new SettingsHolder();
+            var pipelineCache = new PipelineCache(builder, settingsHolder);
 
+            var pipelineConfiguration = new PipelineConfiguration();
+            var pipelineSettings = new PipelineSettings(pipelineConfiguration.Modifications, settingsHolder);
+
+            var pipeline = new Pipeline<ITransportReceiveContext>(builder, settingsHolder, pipelineConfiguration.Modifications);
+
+            var mainPipelineExecutor = new MainPipelineExecutor(builder, eventAggregator, pipelineCache, pipeline);
+
+            var messageContext = new MessageContext("123", new Dictionary<string, string>(), new byte[] { 1, 2, 3 }, new TransportTransaction(), new CancellationTokenSource(), new ContextBag());
+            await mainPipelineExecutor.Invoke(messageContext).ConfigureAwait(false);
         }
     }
 }
